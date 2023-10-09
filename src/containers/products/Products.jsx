b0e9057f38c589b7eadcd12e3cd97202ef9d50de
kasemsun <kasemsun4869@gmail.com>
2023-10-09 11:10:57 +0700

    test suspence

--- src/containers/products/Products.jsx
+++ src/containers/products/Products.jsx
@@ -1,4 +1,5 @@
 import './products.css';
+import React, { Suspense } from 'react';
 import ProductCard from '../../components/productCard/ProductCard';
 import TabProduct from '../../components/tabProduct/TabProduct';
 import { spread_collar_shirt,white_solid_formal_shirt,shine_on_me_blouse,gray_solid_padded_jacket,printed_loose_t_shirt,
@@ -23,9 +24,11 @@ const Product= (props) => {
                 <h2>Our products</h2>
                 <TabProduct/>
                 <section className='fasion__product-card-container'>
+                <Suspense fallback={<div>Loading...</div>}>
                 {products.map((p,index) => (
                     <ProductCard key={index} width={300} name={p.name} price={p.price} star={p.star} image={p.image} />
                 ))}
+                 </Suspense>
                 </section>
             </div>
         </section>
